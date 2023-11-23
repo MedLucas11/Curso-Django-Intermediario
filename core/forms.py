b96fba92d31bebe0a25d067be3dc4b24b60ff94c
Lucas Medeiros da Silva <med_lucassilva@hotmail.com>
2023-11-23 18:54:04 -0300

    Configuração final para envio de emails a partir da página contato

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.core.mail.message import EmailMessage
 
 class ContatoForm(forms.Form):
     nome = forms.CharField(label="Nome")
@@ -7,5 +7,21 @@ class ContatoForm(forms.Form):
     assunto = forms.CharField(label="Assunto")
     mensagem = forms.CharField(label="Mensagem", widget=forms.Textarea())
 
+    def send_mail(self):
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
+        assunto = self.cleaned_data['assunto']
+        mensagem = self.cleaned_data['mensagem']
+
+        conteudo = f'Nome: {nome}\nE-mail: {email}\nAssunto: {assunto}\nMensagem: {mensagem}'
+
+        mail = EmailMessage(
+            subject='E-mail enviado pelo sistema django2',
+            body=conteudo,
+            from_email='contato@seudominio.com.br',
+            to=['contato@seudominio.com.br',],
+            headers={'Reply-To': email}
+        )
+        mail.send()
 
 
